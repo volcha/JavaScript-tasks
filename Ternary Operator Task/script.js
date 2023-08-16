94cab3040f1f37a4ebe6a44a013a7712d1e70086
volcha <57828516+volcha@users.noreply.github.com>
2023-08-16 13:42:57 +0300

    Тернарный оператор

---
+++ Ternary Operator Task/script.js
@@ -0,0 +1,4 @@
+let age = prompt('Enter your age:');
+let oldEnough = age >= 18 ? 'You can buy the alcohol' : 'You can not buy the alcohol, you are too young';
+console.log(oldEnough);
+alert(oldEnough);
