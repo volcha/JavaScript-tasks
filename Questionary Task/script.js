250dbda517e3162560a00b69c4d8e31ecf08087e
volcha <57828516+volcha@users.noreply.github.com>
2023-08-16 13:38:18 +0300

    Задание с prompt

---
+++ Questionary Task/script.js
@@ -0,0 +1,14 @@
+//const favoriteColor = 'blue';
+//alert('My favorite color is ' + favoriteColor);
+
+// prompt - подсказка
+// let favoriteColor;
+// favoriteColor = prompt('What is your favorite color?');
+// console.log('Your favorite coalor is ' + favoriteColor);
+
+const firstName = prompt("What is your name?");
+const surname = prompt("What is your surname?");
+const age = prompt("How old are you?");
+console.log(
+  "This is " + firstName + " " + surname + ", " + age + " years old."
+);
